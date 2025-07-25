1335b0a90f18f3cef4e604ec4a81e62279703243
shubhendu ghosh <shubhendu-ghosh@users.noreply.huggingface.co>
2025-07-25 12:15:51 +0000

    Create app/app.py

---
+++ app/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/product')
+def product():
+    product_id = request.args.get('product_id')
+    appliance = request.args.get('appliance')
+    return render_template('product_detail.html', product_id=product_id, appliance=appliance)
+
+if __name__ == '__main__':
+    app.run(debug=True)
