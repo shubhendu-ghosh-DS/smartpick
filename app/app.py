770f492d7be5632f0b22c0a5101df122dcc902a9
shubhendu ghosh <shubhendu-ghosh@users.noreply.huggingface.co>
2025-07-25 12:31:17 +0000

    Update app/app.py

--- app/app.py
+++ app/app.py
@@ -4,13 +4,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('templates/home.html')
+    return render_template('home.html')
 
 @app.route('/product')
 def product():
     product_id = request.args.get('product_id')
     appliance = request.args.get('appliance')
-    return render_template('templates/product_detail.html', product_id=product_id, appliance=appliance)
+    return render_template('product_detail.html', product_id=product_id, appliance=appliance)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=7860, debug=True)
